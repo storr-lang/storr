30c9906bb3ab59e7ee8c5ae4ac34d141ea1403c6
Yunis Yilmaz <yunis@Yuniss-MacBook-Pro.local>
2024-07-30 23:26:36 -0400

    #2 refactor lexer

--- src/main.cpp
+++ src/main.cpp
@@ -1,82 +1,372 @@
 //
-// Created by Yunis Yilmaz on 2024-07-25.
+// Created by Yunis Yilmaz on 2024-07-30.
 //
 
-#include <string>
-#include <map>
 #include <iostream>
-#include "Lexer.h"
-#include "Token.h"
+#include <fstream>
+#include <map>
+#include <optional>
+#include <sstream>
+#include <vector>
 
-using namespace lexer;
+enum class TokenType : uint_fast8_t {
+    EndOfFile,
+    Illegal,
+    LineSeparator, // (\s*(\r\n|\r|\n))+
+    Comment, // //.*(?=\r\n|\r|\n)
 
-int main() {
-    std::string tokens[] = {
-        "TOKEN_ILLEGAL",
-        "TOKEN_EOF", // -1
-        "TOKEN_COMMENT", // //
+    Identifier, // [_\w][_\w\d]*
+    Integer, // [\d][_\d]*
+    FloatingPoint, // ([\d][_\d]*\.[\d][_\d]*|Infinity|NaN)
+    Boolean, // (True|False)
+    Character, // '.' unimplemented
+    String, // ".*"
 
-        "literal_begin",
-        "TOKEN_IDENTIFIER", // [\w_][\d\w_]*
-        "TOKEN_INT", // \d+
-        "TOKEN_FLOAT", // [\d]+\.[\d]+
-        "TOKEN_BOOL", // (True|False)
-        "literal_end",
+    Add, // +
+    Subtract, // -
+    Multiply, // *
+    Divide, // /
+    Modulo, // %
+    Exponent, // ^
 
-        "operator_begin",
-        "TOKEN_ADD", // +
-        "TOKEN_SUB", // -
-        "TOKEN_MUL", // *
-        "TOKEN_DIV", // /
-        "TOKEN_MOD", // %
-        "TOKEN_EXP", // ^
+    Equals, // =
+    LAssign, // :=
+    RAssign, // =:
+    NEquals, // !=
 
-        "TOKEN_LNOT", // !
-        "TOKEN_LAND", // &
-        "TOKEN_LOR", // |
-        "TOKEN_LNAND", // !&
-        "TOKEN_LNOR", // !|
-        "TOKEN_LXOR", // !=
+    LessThan, // <
+    GreaterThan, // >
+    LTEquals, // <=
+    GTEquals, // >=
 
-        "TOKEN_LT", // <
-        "TOKEN_LTEQUALS", // <=
-        "TOKEN_EQUALS", // =
-        "TOKEN_GTEQUALS", // >=
-        "TOKEN_GT", // >
+    Not, // !
+    And, // &
+    Or, // |
+    NAnd, // !&
+    NOr, // !|
+    Implies, // =>
 
-        "TOKEN_LASSIGN", // :=
-        "TOKEN_RASSIGN", // =:
+    Maplet, // ->
 
-        "TOKEN_COMMA", // ,
-        "TOKEN_COLON", // :
-        "TOKEN_ARROW", // ->
-        "TOKEN_ELIPSIS", // ...
+    LParenthesis, // (
+    RParenthesis, // )
+    LBracket, // [
+    RBracket, // ]
+    LBrace, // {
+    RBrace, // }
+    Colon, // :
+    Comma, // ,
+    Ellipsis, // ...
 
-        "TOKEN_LPAREN", // (
-        "TOKEN_RPAREN", // )
-        "TOKEN_LSQB", // [
-        "TOKEN_RSQB", // ]
-        "TOKEN_LCUB", // {
-        "TOKEN_RCUB", // }
-        "TOKEN_NEWLINE", // \n
-        "operator_end",
-
-        "keyword_begin",
-        "TOKEN_AS",
-        "TOKEN_EXPORT",
-        "TOKEN_FROM",
-        "TOKEN_IMPORT",
-        "TOKEN_MATCH",
-        "keyword_end",
-    };
-
-
-    Token token(Token::Type::keyword_begin, "");
-    while (token.getType() != Token::Type::TOKEN_EOF) {
-        token = Lexer::getNextToken();
-        std::string f = token.getValue();
-        printf("token: %s\nvalue: %s\n\n", tokens[static_cast<int>(token.getType())].c_str(), f.c_str());
-    };
-
-    return 0;
+    As, // as
+    From, // from
+    Import, // import
+    Of, // of
 };
+
+std::vector<std::string> tokens = {
+    "EndOfFile",
+    "Illegal",
+    "LineSeparator",
+    "Comment",
+
+    "Identifier",
+    "Integer",
+    "FloatingPoint",
+    "Boolean",
+    "Character",
+    "String",
+
+    "Add",
+    "Subtract",
+    "Multiply",
+    "Divide",
+    "Modulo",
+    "Exponent",
+
+    "Equals",
+    "LAssign",
+    "RAssign",
+    "NEquals",
+
+    "LessThan",
+    "GreaterThan",
+    "LTEquals",
+    "GTEquals",
+
+    "Not",
+    "And",
+    "Or",
+    "NAnd",
+    "NOr",
+    "Implies",
+
+    "Maplet",
+
+    "LParenthesis",
+    "RParenthesis",
+    "LBracket",
+    "RBracket",
+    "LBrace",
+    "RBrace",
+    "Colon",
+    "Comma",
+    "Ellipsis",
+
+    "As",
+    "From",
+    "Import",
+    "Of",
+};
+
+std::map<std::string, TokenType> identifiers = {
+    {"Infinity", TokenType::FloatingPoint},
+    {"NaN", TokenType::FloatingPoint},
+    {"True", TokenType::Boolean},
+    {"False", TokenType::Boolean},
+    {"as", TokenType::As},
+    {"from", TokenType::From},
+    {"import", TokenType::Import},
+    {"of", TokenType::Of},
+};
+
+std::map<char, TokenType> operators = {
+    {'+', TokenType::Add},
+    {'-', TokenType::Subtract},
+    {'*', TokenType::Multiply},
+    {'/', TokenType::Divide},
+    {'%', TokenType::Modulo},
+    {'^', TokenType::Exponent},
+    {'=', TokenType::Equals},
+    {'<', TokenType::LessThan},
+    {'>', TokenType::GreaterThan},
+    {'!', TokenType::Not},
+    {'&', TokenType::And},
+    {'|', TokenType::Or},
+    {'(', TokenType::LParenthesis},
+    {')', TokenType::RParenthesis},
+    {'[', TokenType::LBracket},
+    {']', TokenType::RBracket},
+    {'{', TokenType::LBrace},
+    {'}', TokenType::RBrace},
+    {':', TokenType::Colon},
+    {',', TokenType::Comma},
+};
+
+struct Token {
+    TokenType type;
+    std::optional<std::string> value;
+};
+
+std::vector<Token> scan(const std::string &content) {
+    std::vector<Token> tokens;
+    for (int i = 0; i < content.length(); i++) {
+        std::string buffer;
+        char c = content.at(i);
+
+        // end of file
+        if (i == content.length() - 1) {
+            tokens.push_back(Token{TokenType::EndOfFile, std::nullopt});
+            break;
+        }
+
+        // ignore whitespace
+        if (isspace(c) && !std::string("\r\n").contains(c) && i != content.length() - 1) continue;
+
+        // line separator
+        if (std::string("\r\n").contains(c)) {
+            do c = content.at(++i);
+            while (isspace(c) && i != content.length() - 1);
+            i--;
+
+            tokens.push_back(Token{TokenType::LineSeparator, std::nullopt});
+            continue;
+        }
+
+        // comment
+        if (c == '/' && content.at(i + 1) == '/') {
+            do {
+                buffer.push_back(c);
+                c = content.at(++i);
+            } while (!std::string("\r\n").contains(c) && i != content.length() - 1);
+            i--;
+
+            tokens.push_back(Token{TokenType::Comment, buffer});
+
+            buffer.clear();
+            continue;
+        }
+
+        // identifier
+        if (c == '_' || isalpha(c)) {
+            do {
+                buffer.push_back(c);
+                c = content.at(++i);
+            } while (c == '_' || isalnum(c));
+            i--;
+
+            if (identifiers.contains(buffer)) tokens.push_back(Token{identifiers[buffer], buffer});
+            else tokens.push_back(Token{TokenType::Identifier, buffer});
+
+            buffer.clear();
+            continue;
+        }
+
+        // numbers
+        if (isdigit(c)) {
+            int dot_count = 0;
+            do {
+                buffer.push_back(c);
+                c = content.at(++i);
+
+                if (c == '.') dot_count++;
+                if (dot_count > 1) {
+                    tokens.push_back(Token{TokenType::Illegal, buffer});
+                    break;
+                }
+            } while (isdigit(c) || c == '_' || c == '.');
+            i--;
+
+            if (buffer.find('.') != std::string::npos) {
+                tokens.push_back(Token{TokenType::FloatingPoint, buffer});
+            } else tokens.push_back(Token{TokenType::Integer, buffer});
+
+            buffer.clear();
+            continue;
+        }
+
+        // character
+        // unimplemented
+        if (c == '\'') {
+            tokens.push_back(Token{TokenType::Illegal, "'"});
+            break;
+        }
+
+        // string
+        if (c == '"') {
+            do {
+                buffer.push_back(c);
+                c = content.at(++i);
+            } while (c != '"');
+            buffer.push_back(c);
+
+            tokens.push_back(Token{TokenType::String, buffer});
+
+            buffer.clear();
+            continue;
+        }
+
+        // operators and delimiters
+        if (c == ':') {
+            c = content.at(++i);
+            if (c == '=') tokens.push_back(Token{TokenType::RAssign, ":="});
+            else {
+                tokens.push_back(Token{TokenType::Colon, ":"});
+                i--;
+            }
+            continue;
+        }
+        if (c == '=') {
+            c = content.at(++i);
+            switch (c) {
+                case ':':
+                    tokens.push_back(Token{TokenType::LAssign, ":="});
+                    break;
+                case '>':
+                    tokens.push_back(Token{TokenType::Implies, "=>"});
+                    break;
+                default:
+                    tokens.push_back(Token{TokenType::Equals, "="});
+                    i--;
+                    break;
+            }
+            continue;
+        }
+        if (c == '!') {
+            c = content.at(++i);
+            switch (c) {
+                case '=':
+                    tokens.push_back(Token{TokenType::NEquals, "!="});
+                    break;
+                case '&':
+                    tokens.push_back(Token{TokenType::NAnd, "!&"});
+                    break;
+                case '|':
+                    tokens.push_back(Token{TokenType::NOr, "!|"});
+                    break;
+                default:
+                    tokens.push_back(Token{TokenType::Not, "!"});
+                    i--;
+                    break;
+            }
+            continue;
+        }
+        if (c == '<') {
+            c = content.at(++i);
+            if (c == '=') tokens.push_back(Token{TokenType::LTEquals, "<="});
+            else {
+                tokens.push_back(Token{TokenType::LessThan, "<"});
+                i--;
+            }
+            continue;
+        }
+        if (c == '>') {
+            c = content.at(++i);
+            if (c == '=')tokens.push_back(Token{TokenType::GTEquals, ">="});
+            else {
+                tokens.push_back(Token{TokenType::GreaterThan, ">"});
+                i--;
+            }
+            continue;
+        }
+        if (c == '-') {
+            c = content.at(++i);
+            if (c == '>') tokens.push_back(Token{TokenType::Maplet, "->"});
+            else {
+                tokens.push_back(Token{TokenType::Subtract, "-"});
+                i--;
+            }
+            continue;
+        }
+        if (c == '.') {
+            c = content.at(++i);
+            if (c == '.' && content.at(++i) == '.') tokens.push_back(Token{TokenType::Ellipsis, "..."});
+            else {
+                tokens.push_back(Token{TokenType::Illegal, std::nullopt});
+                break;
+            }
+            continue;
+        }
+        if (operators.contains(c)) {
+            tokens.push_back(Token{operators[c], std::string(1, c)});
+            continue;
+        }
+
+        // illegal
+        buffer.push_back(c);
+        tokens.push_back(Token{TokenType::Illegal, buffer});
+        break;
+    }
+    return tokens;
+}
+
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        std::cerr << "Usage: " << argv[0] << " <filename>" << std::endl;
+        return 1;
+    }
+
+    std::stringstream content_stream; {
+        std::fstream input(argv[1], std::ios::in);
+        content_stream << input.rdbuf();
+    }
+
+    for (const Token &token: scan(content_stream.str())) {
+        std::cout << "Token: " << tokens[static_cast<int>(token.type)];
+        if (token.value.has_value()) {
+            std::cout << "\nValue: " << token.value.value();
+        }
+        std::cout << "\n" << std::endl;
+    }
+}
